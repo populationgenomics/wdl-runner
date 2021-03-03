e6f0b3f16adc23d3500dd86c2559917d4729203e
pdiakumis <peterdiakumis@gmail.com>
2021-03-03 22:37:35 +1100

    add location

--- wdl_runner/run_lifesciences.sh
+++ wdl_runner/run_lifesciences.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 BUCKET="cpg-peter-dev"
+REGION="australia-southeast1"
 
 gcloud beta lifesciences pipelines run \
     --pipeline-file wdl_pipeline.yaml \
-    --regions australia-southeast1 \
+    --location ${REGION} \
+    --regions ${REGION} \
     --inputs-from-file WDL=test-wdl/ga4ghMd5.wdl,WORKFLOW_INPUTS=test-wdl/ga4ghMd5.inputs.json,WORKFLOW_OPTIONS=test-wdl/basic.papi.australia.options.json \
     --env-vars WORKSPACE=gs://${BUCKET}/wdl_runner/work,OUTPUTS=gs://${BUCKET}/wdl_runner/output \
     --logging gs://${BUCKET}/wdl_runner/logging
